11190cc72fe96062cecb002d60f6a649015f1c91
MohammadWildanAzamZami <43130118+MohammadWildanAzamZami@users.noreply.github.com>
2018-09-10 22:49:22 +0700

    Add files via upload

---
+++ Tugass 1.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugass.pkg1;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Tugass1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    
+       int m =4 ;
+    for (int i=1;i<= m; i++){
+        for (int o=1 ;o<= i; o++){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+}
+    }
